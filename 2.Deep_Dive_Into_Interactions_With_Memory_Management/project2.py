f5820b88de4609217817e5809d41dd87de78836d
Everton Pereira Militao <everton.militao@outlook.com.br>
2024-12-07 20:02:02 -0300

    add ConversationBufferMemory ande ConversationSummaryMemory

--- 2.Deep_Dive_Into_Interactions_With_Memory_Management/project2.py
+++ 2.Deep_Dive_Into_Interactions_With_Memory_Management/project2.py
@@ -1,11 +1,11 @@
 """
 In this project we build a simple chatbot that resolves simple math equations in terminal
 """
-from pydantic import BaseModel
+
 from langchain_groq import ChatGroq
 from langchain_core.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
 from dotenv import load_dotenv
 
 # Secure the api_key
@@ -15,11 +15,21 @@ load_dotenv()
 # Initialize llm
 chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
 
+# With this we can summarize our conversation and return it in a new question.
+memory = ConversationSummaryMemory(
+    memory_key="messages",
+    return_messages=True,
+    llm=chat,
+)
+
+# With this we can store our messages and call back in a new question.
+"""
 memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("messages.json"),
+    chat_memory=FileChatMessageHistory("messages.json"),    # Where we go store our messages
     memory_key="messages",
     return_messages=True
 )
+"""
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
