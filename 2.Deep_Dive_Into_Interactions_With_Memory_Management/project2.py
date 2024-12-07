0b149ced2cf60f19f5c07da482f30483f5d58360
Everton Pereira Militao <everton.militao@outlook.com.br>
2024-12-07 17:54:07 -0300

    updating virtual environment

--- 2.Deep_Dive_Into_Interactions_With_Memory_Management/project2.py
+++ 2.Deep_Dive_Into_Interactions_With_Memory_Management/project2.py
@@ -1,12 +1,12 @@
 """
-In this project we build a simple chatbot that resolves simples math equations in terminal
+In this project we build a simple chatbot that resolves simple math equations in terminal
 """
-
+from pydantic import BaseModel
 from langchain_groq import ChatGroq
-from langchain_core.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain_core.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 from dotenv import load_dotenv
-import argparse
 
 # Secure the api_key
 load_dotenv()
@@ -15,9 +15,16 @@ load_dotenv()
 # Initialize llm
 chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
 
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages",
+    return_messages=True
+)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),      # list of messages that we use to call back that last messages
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
@@ -25,6 +32,7 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm=chat,
     prompt=prompt,
+    memory=memory,
 )
 
 
